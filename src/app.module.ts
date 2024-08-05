c8de204c9a9db0ba7d1ab056c670362217b55e7d
talia-06 <negretenatti26@gmail.com>
2024-08-05 18:02:09 -0500

    cambios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,31 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AutoresModule } from './autores/autores.module';
-import { Autore } from './autores/entities/autore.entity';
 import { GeneroModule } from './genero/genero.module';
+import { ConfigModule } from '@nestjs/config';
+import { Autore } from './autores/entities/autore.entity';
+import { Genero } from './genero/entities/genero.entity';
+
+
 
 @Module({
   imports: [
+     ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env"
+     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306, 
-      username: 'root',
-      password: '',
-      database: 'Music',
+      host: process.env.DB_HOST,
+      port:parseInt( process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE,
+      entities: [Autore,Genero],
       autoLoadEntities: true,
-      synchronize: false,
+      synchronize: true,
     }),
-    AutoresModule,
-    GeneroModule,
+    AutoresModule, GeneroModule,
   ],
 })
 export class AppModule {}
